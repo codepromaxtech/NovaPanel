c8405feff0fe886520859c47f9ea8652dc396f12
codepromaxtech <codepromaxtech@gmail.com>
2026-04-24 07:14:41 +0600

    chore: bump version to v1.0.2

--- backend/internal/version/version.go
+++ backend/internal/version/version.go
@@ -2,4 +2,4 @@ package version
 
 // AppVersion is the running NovaPanel version.
 // Set at build time via: -ldflags "-X github.com/novapanel/novapanel/internal/version.AppVersion=v1.2.3"
-var AppVersion = "v0.6.0"
+var AppVersion = "v1.0.2"
